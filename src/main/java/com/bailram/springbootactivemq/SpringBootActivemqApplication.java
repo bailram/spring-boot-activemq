e2a2139ea41834f17eb2502fc551470980021828
Dhimas Bayu Ilham R <dhimasbayu.9f@gmail.com>
2021-06-07 14:39:10 +0700

    add dependency, create activemq config, create consumer as component, and create produce controller

--- src/main/java/com/bailram/springbootactivemq/SpringBootActivemqApplication.java
+++ src/main/java/com/bailram/springbootactivemq/SpringBootActivemqApplication.java
@@ -2,8 +2,10 @@ package com.bailram.springbootactivemq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.bailram.*")
 public class SpringBootActivemqApplication {
 
 	public static void main(String[] args) {
